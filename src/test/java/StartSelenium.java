07bb3b1b670aeaf10d5e8c36a247fb4ede303068
Maria Z <z0559882272@gmail.com>
2024-06-21 10:33:06 +0300

    add login tests

--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -9,7 +9,15 @@ public class StartSelenium {
     @Test
     public void test() {
         wd = new ChromeDriver();
-        wd.get("https://telranedu.web.app/home");
+        //wd.get("https://telranedu.web.app/home");//without history
+
+        wd.navigate().to("https://telranedu.web.app/home");
+        wd.navigate().back();
+        wd.navigate().forward();
+        wd.navigate().refresh();
+
+        wd.close();
+        wd.quit();
     }
 
 
